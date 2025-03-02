5a078f3aeb4f2cf8c1648f5d1faea6d41ee61dd8
imranBappy <imranbappy.official.com>
2025-03-02 15:41:20 +0600

    static file setting change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -208,7 +208,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Absolute path to where collectstatic will gather the static files
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
